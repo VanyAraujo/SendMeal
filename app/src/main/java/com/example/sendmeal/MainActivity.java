afdfad45c21c7915db2a7c8d2639f29839909520
VanyAraujo <araujo.g.vanina@gmail.com>
2020-09-24 21:09:39 -0300

    validaciones de campos

--- app/src/main/java/com/example/sendmeal/MainActivity.java
+++ app/src/main/java/com/example/sendmeal/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.graphics.Region;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.util.Patterns;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -18,6 +19,10 @@ import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
+import java.util.regex.Pattern;
+
+import static android.util.Patterns.*;
+
 public class MainActivity extends AppCompatActivity {
 
     public EditText txtNombreyApellido;
@@ -27,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
     public RadioButton rbtTarjetaDebito;
     public RadioButton rbtTarjetaCredito;
     public EditText txtCbu;
-    public EditText txtNumeroTarjeta,txtCvv;
+    public EditText txtNumeroTarjeta, txtCvv;
     public Button btnRegistar;
     public CheckBox cbxTermino;
     public SeekBar seekBar;
@@ -61,7 +66,7 @@ public class MainActivity extends AppCompatActivity {
         rbtTarjetaCredito = findViewById(R.id.rbtTarjetaCredito);
         boolean rbtTarjetaCreditoResponse = rbtTarjetaDebito.isChecked();
 
-        seekBar= findViewById(R.id.seekBar);
+        seekBar = findViewById(R.id.seekBar);
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
@@ -78,36 +83,64 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    
 
-        btnRegistar.setOnClickListener( new OnClickListener()
-        {
-            @Override
-            public void onClick(View view) {
-                Context context = getApplicationContext();
-                CharSequence text = "Error existen campos vacios";
-                int duration = Toast.LENGTH_SHORT;
+        btnRegistar.setOnClickListener(new
 
-                if(txtNombreyApellido.getText().toString().isEmpty()){
-                    Toast.makeText(context, text, duration).show();
-                }else
-                if(txtContrasenia.getText().toString().isEmpty()) {
-                    Toast.makeText(context,text,duration).show();
-                }else
-                if(txtRepetirContrasenia.getText().toString().isEmpty()){
-                    Toast.makeText(context,text,duration).show();
-                }else if(txtCbu.getText().toString().isEmpty()) {
-                    Toast.makeText(context, text, duration).show();
+    OnClickListener() {
+        @Override
+        public void onClick (View view){
+            Context context = getApplicationContext();
+            CharSequence text = "Error existen campos vacios";
+            int duration = Toast.LENGTH_SHORT;
 
-                }else if (txtCvv.getText().toString().isEmpty())
-                    Toast.makeText(context, text, duration).show();
-            }
+            if (txtNombreyApellido.getText().toString().isEmpty()) {
+                Toast.makeText(context, text, duration).show();
+            } else if (txtContrasenia.getText().toString().isEmpty()) {
+                Toast.makeText(context, text, duration).show();
+            } else if (txtRepetirContrasenia.getText().toString().isEmpty()) {
+                Toast.makeText(context, text, duration).show();
+            } else if (txtCbu.getText().toString().isEmpty()) {
+                Toast.makeText(context, text, duration).show();
+
+            } else if (txtCvv.getText().toString().isEmpty())
+                Toast.makeText(context, text, duration).show();
+
+
             String NombreyApellido = txtNombreyApellido.getText().toString();
             String contrasenia = txtContrasenia.getText().toString();
             String repetirContrasenia = txtRepetirContrasenia.getText().toString();
+
+            if ((contrasenia.equals(repetirContrasenia)) == false)
+                Toast.makeText(context, "Las contraseñas ingresadas no coinciden", duration).show();
+
             String email = txtEmail.getText().toString();
+            boolean mailCorrecto;
+
+            {
+                mailCorrecto = isValidEmail(email);
+            }
+
+            if (mailCorrecto == false) {
+                Toast.makeText(context, "El mail ingresado no tiene un formato correcto", duration).show();
+            }
+
             String numeroTarjeta = txtNumeroTarjeta.getText().toString();
             String cbu = txtCbu.getText().toString();
             String cvv = txtCvv.getText().toString();
-        });
+
+        }
+
+    });
 }
-}
+
+
+
+        public final static boolean isValidEmail (String email){
+            if (email == null)
+                return false;
+            return EMAIL_ADDRESS.matcher(email).matches();
+        }
+
+
+    }
